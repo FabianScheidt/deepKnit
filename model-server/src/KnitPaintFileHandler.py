b292a8094dd54ffced8da1d3712d0da6d9dfea88
Andre Schmeing <andre.schmeing@ima-ifu.rwth-aachen.de>
2018-12-11 18:49:22 -0500

    Add first version of basic syntax checks to KnitPaintFileHandler.py

--- model-server/src/KnitPaintFileHandler.py
+++ model-server/src/KnitPaintFileHandler.py
@@ -55,6 +55,166 @@ class KnitPaintFileHandler:
         [0, 175, 0], [0, 150, 0], [200, 0, 0], [110, 0, 0], [100, 100, 100], [0, 125, 0]
     ]
 
+    # Default color labels
+    color_labels = [
+        'Miss',
+        'Front knit (jersey)',
+        'Back knit (reversed jersey)',
+        'Front / back knit',
+        'Normal stich knit (lower) [Front stich cross(1)]',
+        'Normal stich knit (upper) [Front stich cross(1)] / [Front-Back stich cross(1)]',
+        'Front knit + move 1P left',
+        'Front knit + move 1P right',
+        'Back knit + move 1P left',
+        'Back knit + move 1P right',
+        'Reverse stich knit (lower) [Front-Back stich cross(1)]',
+        'Front tuck',
+        'Back tuck',
+        'Auto yarn feeder point',
+        'Normal stich knit (lower) [Front stich cross(2)]',
+        'Normal stich knit (upper) [Front stich cross(2)] / [Front-Back stich cross(2)]',
+        'No needle selection',
+        'Front knit (with kick back process)',
+        'Back knit (with kick back process)',
+        '',
+        'Front knit + transfer',
+        'Front knit + transfer 1P left',
+        'Front knit + transfer 2P left',
+        'Front knit + transfer 3P left',
+        'Front knit + transfer 1P right',
+        'Front knit + transfer 2P right',
+        'Front knit + transfer 3P right',
+        'Front knit for 2nd stich (-)',
+        'Back knit for 2nd stich (-)',
+        'Front knit + transfer',
+        'Back knit + transfer',
+        'Back knit + transfer 1P left',
+        'Back knit + transfer 2P left',
+        'Back knit + transfer 3P left',
+        'Back knit + transfer 1P right',
+        'Back knit + transfer 2P right',
+        'Back knit + transfer 3P right',
+        'Front knit for 2nd stich (+)',
+        'Back knit for 2nd stich (+)',
+        'Back knit + transfer',
+        'Front knit + transfer',
+        'Front knit, knit + receive assistance (back) (without links process)',
+        'Knit + receive assistance (front), back knit (without links process)',
+        'Front knit + transfer 4P left',
+        'Front knit + transfer 5P left',
+        'Front knit + transfer 6P left',
+        'Front knit + transfer 7P left',
+        'Front knit + transfer 4P right',
+        'Front knit + transfer 5P right',
+        'Front knit + transfer 6P right',
+        'Back knit + transfer',
+        'Front knit (without links process)',
+        'Back knit (without links process)',
+        'Back knit + transfer 4P left',
+        'Back knit + transfer 5P left',
+        'Back knit + transfer 6P left',
+        'Back knit + transfer 7P left',
+        'Back knit + transfer 4P right',
+        'Back knit + transfer 5P right',
+        'Back knit + transfer 6P right',
+        '',
+        'Front knit + move 1P left',
+        'Front knit + move 2P left',
+        'Front knit + move 3P left',
+        'Front knit + move 4P left',
+        'Front miss + move 1P left',
+        'Front miss + move 2P left',
+        'Front miss + move 4P left',
+        'Front knit + transfer 7P right',
+        'Back knit + transfer 7P right',
+        '',
+        'Front knit + move 1P right',
+        'Front knit + move 2P right',
+        'Front knit + move 3P right',
+        'Front knit + move 4P right',
+        'Front miss + move 1P right',
+        'Front miss + move 2P right',
+        'Front miss + move 4P right',
+        '', '', '',
+        'Back knit + move 1P left',
+        'Back knit + move 2P left',
+        'Back knit + move 3P left',
+        'Back knit + move 4P left',
+        'Back miss + move 1P left',
+        'Back miss + move 2P left',
+        'Back miss + move 4P left',
+        'Knit + receive assistance (front and back)',
+        '', '',
+        'Back knit + move 1P right',
+        'Back knit + move 2P right',
+        'Back knit + move 3P right',
+        'Back knit + move 4P right',
+        'Back miss + move 1P right',
+        'Back miss + move 2P right',
+        'Back miss + move 4P right',
+        '',
+        'Links process code + Pulldown pattern output code (255)',
+        'Reverse stich knit (lower) [Front-Back stich cross(2)]',
+        'Front split knit',
+        'Back split knit',
+        'Transfer + front knit for 2nd stich (-) + transfer',
+        'Back knit + transfer (non auto-transfer)',
+        '',
+        'Front split knit + transfer 1P left',
+        'Front split knit + transfer 1P right',
+        'Back split knit + transfer 1P left',
+        'Back split knit + transfer 1P right',
+        '',
+        'Front tuck for 2nd stich (-)',
+        'Back tuck for 2nd stich (-)',
+        '',
+        'Front knit (out of commanded bed)(without links process)',
+        'Back knit (out of commanded bed) (without links process)',
+        'Front miss (with links process)',
+        'Back miss (with links process)',
+        'Front knit (out of commanded bed) (not intended for receive assistance)',
+        'Back knit (out of commanded bed) (not intended for receive assistance)',
+        'Front knit (out of commanded bed) + transfer (OP line L13: back body)',
+        'Color no. 1 + Auto yarn feeder point',
+        'Color no. 2 + Auto yarn feeder point',
+        'Color no. 3 + Auto yarn feeder point',
+        'Color no. 4 + Auto yarn feeder point',
+        '',
+        'Front split knit + transfer 2P left',
+        'Front split knit + transfer 2P right',
+        'Back split knit + transfer 2P left',
+        'Back split knit + transfer 2P right',
+        'Back knit (out of commanded bed) + transfer (OP Lline L13: front body)',
+        'Front knit (without miss TR)',
+        'Back knit (without miss TR)',
+        '', '',
+        'Front / back move 1 pitch left',
+        'Front / back move 1 pitch right',
+        '', '', '', '', '', '', '', '',
+        'Front / back move 2 pitch left',
+        'Front / back move 2 pitch right',
+        '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+        'Front knit (jersey) Knitting assist prohibition (without links process)',
+        'Back knit (reversed jersey) Knitting assist prohibition (without links process)',
+        '', '', '', '', '', '', '', '',
+        'Front tuck (without links process)',
+        'Back tuck (without links process)',
+        'Front tuck for 2nd stich (without links process)',
+        'Back tuck for 2nd stich (without links process)',
+        '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '',
+        '', '', '', '', '', '',
+        'Pulldown pattern shift knit code',
+        '',
+        'Pulldown pattern output code',
+        'Pulldown pattern output code',
+        'Pulldown pattern output code',
+        '',
+        'Pulldown pattern output code'
+    ]
+
     # Default resolution
     resolution_valid = 0
     resolution_x_den = 0
@@ -394,3 +554,151 @@ class KnitPaintFileHandler:
             file.write(dat_bytes)
             file.close()
             print('Saved dat to ' + output_filename)
+
+    def find_categories_by_keyword(self, keywords):
+        """
+        Checks the color_labels for matches with the keywords
+        :param char:
+        :return: Indices of color_labels, that contain at least one of the keywords.
+                 If no match is found [-1] is returned.
+        """
+
+        # Search for matches of every keyword in color_label list and append to color_label_indices
+        color_label_indices = []
+        for keyword in keywords:
+            for label_index in range(len(self.color_labels)):
+
+                if keyword in self.color_labels[label_index]:
+                    color_label_indices.append(label_index)
+
+        # Delete duplicates in the labels to receive a unique index listing
+        color_label_indices = np.unique(color_label_indices)
+
+        # Return -1 if no keyword match was found in the labels, else return the label indices
+        if len(color_label_indices == 0):
+            return [-1]
+        else:
+            return color_label_indices
+
+    def bitmap_syntax_check(self, maximum_tucks: int):
+        """
+        Checks current bitmap data for syntactical correctness
+        :param char:
+        :return: True if syntax correct, False if syntax is incorrect
+        """
+        syntax_check = True
+        bitmap_width = self.get_width()
+        bitmap_height = self.get_height()
+        bitmap_np = np.array(self.bitmap_data)
+        bitmap_np_shaped = np.reshape(bitmap_np, (bitmap_height, bitmap_width))
+
+        # Check1: Knit: 1: Total courses must be even number
+        if not bitmap_height % 2 == 0:
+            syntax_check = False
+            print('Error: Current Bitmap data did not pass the syntax check: '
+                  + 'Total courses number is ' + str(bitmap_height) + ' and should be an even number.')
+
+        # Check2: Knit: 2: Always check carrier position after ending
+        # ...
+
+        # Check3: Tuck: 1: Do not tuck on the ending needle
+        for line_index in range(bitmap_height):
+            # Check if there is a tuck on an ending needle for each line in the bitmap data
+            if (
+                    bitmap_np_shaped[line_index][0] in self.find_categories_by_keyword(['tuck'])
+                    or bitmap_np_shaped[line_index][self.get_width() - 1] in self.find_categories_by_keyword(['tuck'])
+            ):
+                syntax_check = False
+                print('Error: Current Bitmap data did not pass the syntax check: '
+                      + 'Tuck on ending needle in numpy bitmap line ' + str(line_index + 1) + ' detected. '
+                      + 'Do not tuck on the ending needle')
+
+        # Check4: Tuck: 2: Do not tuck more than x (depends on yarn)
+        for column_index in range(bitmap_width):
+            tucks_in_row = 0
+            for line_index in range(bitmap_height):
+
+                # Sum up the tucks appended continuous in a column
+                if (bitmap_np_shaped[line_index][column_index] in self.find_categories_by_keyword(['tuck'])):
+                    tucks_in_row += 1
+                else:
+                    tucks_in_row = 0
+
+                # If number of continuous tucks exceeds the maximum number, the syntax is not correct
+                if tucks_in_row > maximum_tucks:
+                    syntax_check = False
+                    print('Error: Current Bitmap data did not pass the syntax check: '
+                          + 'Maximum number of tucks in row exceeded in numpy bitmap column ' + str(line_index + 1)
+                          + ' and line ' + str(column_index + 1) + '. Do not do more tucks than ' + str(maximum_tucks)
+                          + ' per column.')
+
+        # Check5: Miss: 1: if miss >= course; Color0 = Color16; Color13 = Color16
+        for line_index in range(3, bitmap_height-3):
+            # Check if there is a whole line of misses in the bitmap data, that was not set to Color16
+            # and the whole line of misses is not in the first or last 3 lines of the normalized Bitmap data
+            if(
+                (bitmap_np_shaped[line_index] == [0]*bitmap_width).all()
+                or (bitmap_np_shaped[line_index] == [13]*bitmap_width).all()
+            ):
+                syntax_check = False
+                print('Error: Current Bitmap data did not pass the syntax check: '
+                      + 'Complete line of misses in line ' + str(line_index+1) + ' detected. '
+                      + 'Do not do only misses in a line with \'miss\' or \'auto yarn feeder point\'.'
+                      + 'Should have been set to \'no needle selection\' before knitting.')
+
+        # Check6: Transfer/Receive: 1: In stitch move, the overlapped and pick up stitch might be unstable
+        # ...
+
+        # Check7: Transfer/Receive: 2: Rack_Right -> transfter_Single -> rack_Left -> transfer_Double is unstable. Do:
+        # 	ack_Left ->transfter_Single -> rack_Right -> transfer_Single
+        # ...
+
+        # Check8: Transfer / Receive: 3: Use Color30/20 to link from two bed to one bed
+        # ...
+
+        # Check9: Transfer / Receive: 4: Use Color30/20 to link from two bed to one bed
+        # ...
+
+        # Check10: Transfer / Receive: 5: Color80/90 override the option line R9.
+        #                                Do not draw the line only with Color80/90
+        # ...
+
+        # Check11: Racking: 1: Racking for transfer does not need to specify on L2, L3, L4
+        # ...
+
+        # Check12: Racking: 2: Cross stitch comes in pair
+        # Defining the labels of cross stitches that have to be in pairs for set1 and set2
+        color_labels_cross_lower_1 = [4, 10]
+        color_labels_cross_upper_1 = [5]
+        color_labels_cross_lower_2 = [14, 100]
+        color_labels_cross_upper_2 = [15]
+        for line_index in range(bitmap_height):
+            # Check if cross stitches are in the current line of numpy Bitmap data and set the corresponding boolean
+            cross_lower_set_1 = False
+            cross_upper_set_1 = False
+            cross_lower_set_2 = False
+            cross_upper_set_2 = False
+
+            for color_label in color_labels_cross_lower_1:
+                if (color_label in bitmap_np_shaped[line_index]):
+                    cross_lower_set_1 = True
+
+            for color_label in color_labels_cross_upper_1:
+                if (color_label in bitmap_np_shaped[line_index]):
+                    cross_upper_set_1 = True
+
+            for color_label in color_labels_cross_lower_2:
+                if (color_label in bitmap_np_shaped[line_index]):
+                    cross_lower_set_2 = True
+
+            for color_label in color_labels_cross_upper_2:
+                if (color_label in bitmap_np_shaped[line_index]):
+                    cross_upper_set_2 = True
+
+            # If any cross stitch does not appear in a pair, the syntax is not correct
+            if (cross_lower_set_1 ^ cross_upper_set_1) or (cross_lower_set_2 ^ cross_upper_set_2):
+                syntax_check = False
+                print('Error: Current Bitmap data did not pass the syntax check: '
+                      + 'Single category of cross stitch in line ' + str(line_index + 1) + ' detected. '
+                      + 'Cross stitches have to come in pairs.')
+        return syntax_check
