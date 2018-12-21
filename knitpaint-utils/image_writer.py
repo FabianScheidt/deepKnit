55aaf4adb38d0f310152b8ddac42f4c079c264e6
Fabian Scheidt <fabianscheidt@yahoo.de>
2018-12-21 18:32:26 -0500

    Refactor KnitPaintFileHandler to proper python module

---
+++ knitpaint-utils/image_writer.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+
+def write_image(src, output_filename, verbose=True):
+    """
+    Builds a preview image of the provided KnitPaint file using Numpy and OpenCV
+    :param src: KnitPaint source
+    :param output_filename:
+    :param verbose: Set to False to disable logging on success
+    :return:
+    """
+    bitmap_np = src.get_np_bitmap_data()
+    bitmap_lut = np.array(src.color_table)
+    image_rgb = bitmap_lut[bitmap_np]
+    image_bgr = image_rgb[:, :, ::-1]
+    image_bgr_flipped = cv2.flip(image_bgr, 0)
+    cv2.imwrite(output_filename, image_bgr_flipped)
+    if verbose:
+        print('Saved preview image to ' + output_filename)
