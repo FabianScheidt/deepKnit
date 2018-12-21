3d237feba17b25fea7e67fa4f43f770e910f40a7
Fabian Scheidt <fabianscheidt@yahoo.de>
2018-12-21 18:57:47 -0500

    Add support to read lep files

---
+++ model-server/src/knitpaint/lep_reader.py
@@ -0,0 +1,9 @@
+def read_lep(input_filename, dst=None):
+    """
+    Reads all information from the provided lep file
+
+    :param input_filename:
+    :param dst:
+    :return:
+    """
+    raise NotImplementedError('Confidential')
