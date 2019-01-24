81c6c413f380cdfe16632520043de49ca76a5fbe
Fabian Scheidt <fabianscheidt@yahoo.de>
2019-01-23 23:06:55 -0500

    Add keyboard shortcuts for undo and redo

--- frontend/src/app/editor/editor.component.ts
+++ frontend/src/app/editor/editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { fromEvent, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { EditorStateService } from './editor-state.service';
 
 @Component({
   selector: 'app-editor',
@@ -9,27 +10,47 @@ import { takeUntil } from 'rxjs/operators';
 })
 export class EditorComponent implements OnInit, OnDestroy {
 
-  private destroySubject: Subject<void> = new Subject<void>();
+  private onDestroy: Subject<void> = new Subject<void>();
 
-  constructor() { }
+  constructor(private editorStateService: EditorStateService) { }
 
   ngOnInit() {
     this.preventGestures();
+    this.registerShortcuts();
   }
 
   ngOnDestroy(): void {
-    this.destroySubject.next();
+    this.onDestroy.next();
   }
 
   /**
    * Prevents standard behaviour of gestures
    */
   private preventGestures() {
-    fromEvent(document, 'gesturestart').pipe(takeUntil(this.destroySubject)).subscribe((e) => {
+    fromEvent(document, 'gesturestart').pipe(takeUntil(this.onDestroy)).subscribe((e) => {
       e.preventDefault();
     });
   }
 
+  /**
+   * Registers shortcuts for undo and redo
+   */
+  private registerShortcuts() {
+    fromEvent(window, 'keydown').pipe(takeUntil(this.onDestroy)).subscribe((e: KeyboardEvent) => {
+      if (e.key === 'z' && (e.metaKey || e.ctrlKey) && !e.shiftKey) {
+        this.editorStateService.undo();
+        e.preventDefault();
+      }
+      if (e.key === 'z' && (e.metaKey || e.ctrlKey) && e.shiftKey) {
+        this.editorStateService.redo();
+        e.preventDefault();
+      }
+    });
+  }
+
+  /**
+   * Returns if the current browser supports touch events
+   */
   public isTouchDevice() {
     return 'ontouchstart' in window;
   }
