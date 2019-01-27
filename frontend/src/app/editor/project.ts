a9f09c32f9dab8e8927e8a2dcdc6edb4d9b95a5e
Fabian Scheidt <fabianscheidt@yahoo.de>
2019-01-27 18:19:56 -0500

    Add a uuid to projects

--- frontend/src/app/editor/project.ts
+++ frontend/src/app/editor/project.ts
@@ -1,4 +1,5 @@
 import { Knitpaint } from '../knitpaint';
+import * as uuidv4 from 'uuid/v4';
 
 export enum ProjectStage {
   Setup = 0,
@@ -14,15 +15,17 @@ export class Project {
   public readonly stage: ProjectStage = ProjectStage.Setup;
   public readonly patterns: Knitpaint[] = [];
   public readonly assembly: Knitpaint;
+  public readonly uuid: string;
 
   public static fromJSON(json: any): Project {
     const stage = json.stage;
     const patterns = json.patterns.map((pattern) => Knitpaint.fromJSON(pattern));
     const assembly = Knitpaint.fromJSON(json.assembly);
-    return new Project(stage, patterns, assembly);
+    const uuid = json.uuid;
+    return new Project(stage, patterns, assembly, uuid);
   }
 
-  constructor(stage?: ProjectStage, patterns?: Knitpaint[], assembly?: Knitpaint) {
+  constructor(stage?: ProjectStage, patterns?: Knitpaint[], assembly?: Knitpaint, uuid?: string) {
     if (stage) {
       this.stage = stage;
     }
@@ -37,17 +40,18 @@ export class Project {
       const data = new Uint8Array(width * height);
       this.assembly = new Knitpaint(data.buffer, width);
     }
+    this.uuid = uuid || uuidv4();
   }
 
   setStage(stage: ProjectStage) {
-    return new Project(stage, this.patterns, this.assembly);
+    return new Project(stage, this.patterns, this.assembly, this.uuid);
   }
 
   setPatterns(patterns: Knitpaint[]) {
-    return new Project(this.stage, patterns, this.assembly);
+    return new Project(this.stage, patterns, this.assembly, this.uuid);
   }
 
   setAssembly(assembly: Knitpaint) {
-    return new Project(this.stage, this.patterns, assembly);
+    return new Project(this.stage, this.patterns, assembly, this.uuid);
   }
 }
