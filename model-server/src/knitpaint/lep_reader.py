a0931adf19cfc72f6fd18eade16d3131d5c65d47
Fabian Scheidt <fabianscheidt@yahoo.de>
2020-12-06 22:13:40 +0100

    Move knitpaint handling to a different repository

--- model-server/src/knitpaint/lep_reader.py
+++
@@ -1,9 +0,0 @@
-def read_lep(input_filename, dst=None):
-    """
-    Reads all information from the provided lep file
-
-    :param input_filename:
-    :param dst:
-    :return:
-    """
-    raise NotImplementedError('Confidential')
